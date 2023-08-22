775863afa27979461cbfc726a558095d46caab34
shimano05 <ramiramara2019@gmail.com>
2023-08-22 19:11:57 +0900

    ♻️ 使用していないステートのコメントアウト

--- next-app/pages/index.tsx
+++ next-app/pages/index.tsx
@@ -4,17 +4,17 @@ import AgentInfoUI from "@/components/agentInfoUI";
 import React, { useEffect, useState } from "react";
 
 export default function Home() {
-  const [size, setSize] = useState({
-    width: 0,
-    height: 0,
-  });
+  // const [size, setSize] = useState({
+  //   width: 0,
+  //   height: 0,
+  // });
 
   const [Loading, setLoading] = useState<boolean>(false);
 
   const [data, setData] = useState<string[]>([]);
 
   useEffect(() => {
-    setSize({ width: window.screen.width, height: window.screen.height });
+    // setSize({ width: window.screen.width, height: window.screen.height });
     setLoading(true);
   }, []);
 
